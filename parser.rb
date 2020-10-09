156f026ab6b149184974887d0931cd47f5de229a
Petr Zaparka <petr@zaparka.cz>
2020-10-09 17:39:05 +0100

    Parsing paragraphs and links to kindle

--- parser.rb
+++ parser.rb
@@ -6,23 +6,49 @@
 require 'nokogiri'
 require 'date'
 require 'ostruct'
+# require 'pry'
 
 class Note < OpenStruct; end
 class Notes < Array; end
 
 notes = Notes.new
-
+filename = ARGV[0]
 xml = Nokogiri::XML(File.open(ARGV[0]))
+
 xml.xpath("//note").each do |n|
   note = Note.new
   note.title = n.xpath('title').first.content
   note.content_xml = n.xpath('content').first.content
   note.content = Nokogiri::XML(note.content_xml).content
-  note.created = DateTime.parse(n.xpath('created').first.content)
-  note.updated = DateTime.parse(n.xpath('updated').first.content)
+
+  note.created = DateTime.parse(n.xpath('created').first.content).strftime("%Y-%m-%d %H:%M %Z")
+  note.updated = DateTime.parse(n.xpath('updated').first.content).strftime("%Y-%m-%d %H:%M %Z")
   note.tags = n.xpath('tag').map(&:content)
-  note.attributes = n.xpath('note-attributes')
-                     .children
-                     .inject({}){|h, i| h[i.name] = i.content ; h }
+
   notes << note
+  text = n.elements[1].children.first.text
+  doc = Nokogiri::HTML(text)
+
+  md_filename = filename.gsub('.enex','.md')
+  File.open(md_filename, 'w') do |file|
+    file.write "# #{note.title}\n"
+    file.write "Title: #{note.title}\n"
+    file.write "Created: #{note.created}\n"
+    file.write "Updated: #{note.updated}\n"
+    file.write "\n\n\n"
+
+    doc.xpath('//div[contains(@style, "-en-paragraph")]').each_with_index do |paragraph, i|
+      next if i == 0
+
+      paragraph.children.each do |child|
+        if child.has_attribute?('href')
+          file.write "[#{child.text}](#{child.attribute('href').text})\n"
+        else
+          text = child.text.delete_suffix('(')
+          text = text.delete_prefix(')')
+          file.write "#{text}\n"
+        end
+      end
+    end
+  end
 end
